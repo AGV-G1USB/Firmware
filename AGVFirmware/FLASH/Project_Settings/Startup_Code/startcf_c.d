38f37803b919e2aaa7a4864343a432c9a72d37ba
Jeck Jaimes <jeckjaimes@gmail.com>
2018-05-02 18:38:11 -0400

    Firmware CodeWarrior

---
+++ AGVFirmware/FLASH/Project_Settings/Startup_Code/startcf_c.d
@@ -0,0 +1,6 @@
+Project_Settings/Startup_Code/startcf_c.obj: C:\Users\Jeck\ Jaimes\Documents\Jeckson\ USB\Trimestre\ XII\EC3883\ -\ Laboratorio\ de\ Proyectos\ III\Proyecto\ GitHub\Firmware\AGVFirmware\Project_Settings\Startup_Code\startcf.c \
+	C:\Freescale\CW\ MCU\ v10.5\MCU\ColdFire_Support\ewl\EWL_C\include\lib_ewl.prefix \
+	C:\Freescale\CW\ MCU\ v10.5\MCU\ColdFire_Support\ewl\EWL_C\include\common.prefix \
+	C:\Freescale\CW\ MCU\ v10.5\MCU\ColdFire_Support\ewl\EWL_C\include\ewl_c_version.h \
+	C:\Users\Jeck\ Jaimes\Documents\Jeckson\ USB\Trimestre\ XII\EC3883\ -\ Laboratorio\ de\ Proyectos\ III\Proyecto\ GitHub\Firmware\AGVFirmware\Project_Settings\Startup_Code\startcf.h \
+	C:\Freescale\CW\ MCU\ v10.5\MCU\ColdFire_Support\ewl\EWL_Runtime\Runtime_ColdFire\Source\RuntimeConfig.h 
